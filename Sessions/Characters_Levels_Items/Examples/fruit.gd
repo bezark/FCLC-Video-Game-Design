28a44acf971bd586aeb9aa9ca854eb74eed9f4f4
bezark <jdb459@nyu.edu>
2024-02-25 17:13:22 -0500

    character level item

---
+++ Sessions/Characters_Levels_Items/Examples/fruit.gd
@@ -0,0 +1,20 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_2d_body_entered(body):
+	play("pop")
+
+
+func _on_animation_finished():
+	if animation == "pop":
+		queue_free()
