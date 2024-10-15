ce045bfac3f0813d083623cdb312d694881da0b9
bezark <johnbezark@gmail.com>
2024-10-15 13:49:33 -0400

    In class

--- Modules/2D/Platformer-Tilemap/platformer/character_body_2d.gd
+++ Modules/2D/Platformer-Tilemap/platformer/character_body_2d.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -200.0
+var jumping = false
 
 @onready var animated_sprite_2d = get_node("AnimatedSprite2D")
 
@@ -14,20 +15,23 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.play("jump")
+		jumping = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		animated_sprite_2d.play("walk")
 		velocity.x = direction * SPEED
-	
-		if direction == -1:
-			animated_sprite_2d.flip_h = true
-		else:
-			animated_sprite_2d.flip_h = false
+		if not jumping:
+			animated_sprite_2d.play("walk")
+			if direction == -1:
+				animated_sprite_2d.flip_h = true
+			else:
+				animated_sprite_2d.flip_h = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animated_sprite_2d.play("default")
+		if not jumping:
+			animated_sprite_2d.play("default")
 
 	move_and_slide()
