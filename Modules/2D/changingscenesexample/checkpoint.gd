85d8c3f2c1e24f3e16d14791cc8928d27b5d5e8c
bezark <johnbezark@gmail.com>
2024-10-15 14:16:10 -0400

    in class demo

--- Modules/2D/changingscenesexample/checkpoint.gd
+++ Modules/2D/changingscenesexample/checkpoint.gd
@@ -16,6 +16,7 @@ func _process(delta):
 func _on_area_2d_body_entered(body):
 	print(body)
 	print("thing was triggered")
+	#get_tree().change_scene_to_file("res://home_base.tscn")
 	get_tree().change_scene_to_packed(scene_to_load)
 	
 	
