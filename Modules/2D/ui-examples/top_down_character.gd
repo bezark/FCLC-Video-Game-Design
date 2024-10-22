51900cab13cd5f1f7caf45f6437cf805212ee3dc
bezark <johnbezark@gmail.com>
2024-10-22 12:58:21 -0400

    UI Demo

---
+++ Modules/2D/ui-examples/top_down_character.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+@export var speed = 10
+var score = 0
+func _process(delta):
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= speed
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y += speed
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x -= speed
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x += speed
+	else:
+		velocity = Vector2(0,0)
+		pass
+	move_and_slide()
+
+func change_score(amount):
+	score += amount
+	$CanvasLayer/HBoxContainer/Score.text = str(score)
+	
